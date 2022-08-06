65af4a437b265d78cf335cbee0caa62380847544
Сергей <sergeyandreev22@gmail.com>
2022-08-07 02:46:38 +0300

    8 Task Completed

--- 8 Task/Program.cs
+++ 8 Task/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine()); // Ввод числа
+int i = 2; // Для цикла, по условиям задачи, логически
+
+Console.WriteLine("Если число: "); // Цикл
+while (i < num)
+{
+    Console.WriteLine(i); // Печатаем в консоль следующее четное число
+    i += 2; // Инкрементируем на двойку
+}
