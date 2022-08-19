217df5fc2060d4de68fbe37ae59a0513bc1c76c7
Сергей <sergeyandreev22@gmail.com>
2022-08-20 00:54:09 +0300

    Task 27 DONE

---
+++ 27 Task/Program.cs
@@ -0,0 +1,12 @@
+﻿/*  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. */
+Console.WriteLine("Введите число, сумму цифр которого нужно посчитать");
+int Number = int.Parse(Console.ReadLine());
+    int sum = 0;
+    while (Number > 0)
+    {
+        sum = sum + Number % 10;
+        Number = Number/10;
+    }
+Console.ForegroundColor = ConsoleColor.Green;  
+Console.WriteLine(sum);
+Console.ResetColor();
