25779514657ce03d6961bfe7e888b2bcff706cb0
Сергей <sergeyandreev22@gmail.com>
2022-08-20 00:42:31 +0300

    Task 25 DONE

---
+++ 25 Task/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. */
+
+Console.WriteLine("Введите число");
+int Number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите cтепень, в которую необходимо возвести число");
+int Degree = Convert.ToInt32(Console.ReadLine());
+
+double NumberX3 = Math.Pow(Number,Degree);
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write(NumberX3 + "\t");
+Console.ResetColor();
