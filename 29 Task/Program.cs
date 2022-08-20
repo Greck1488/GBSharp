24f22250b42e3977158a292f1f247099bedfa56f
Сергей <sergeyandreev22@gmail.com>
2022-08-20 14:07:56 +0300

    Task 29 DONE

---
+++ 29 Task/Program.cs
@@ -0,0 +1,11 @@
+﻿/*  Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. */
+Console.WriteLine("Введите восемь через пробел и нажмите Enter");
+int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+Console.WriteLine("Окей! Вы ввели следующие числа: ");
+Console.ForegroundColor = ConsoleColor.Green; 
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write("| " + numbers[i] + " |");
+}
+Console.ResetColor();
